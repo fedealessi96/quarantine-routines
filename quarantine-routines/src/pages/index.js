615b82e2890919e15c558bc549902712c96458f9
Federico <federico.alessi@me.com>
2020-03-18 21:08:00 +0100

    First commit

---
+++ quarantine-routines/src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1></h1>
+    <p>Hey there! </p>
+    <p>The only thing you will see here will be what I do (or did) during Italy's lockdown.</p>
+
+
+
+  </Layout>
+);
+
+export default IndexPage
