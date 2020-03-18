b99fa6b5e131c3177c06fe6f9b79648379c61629
Federico <federico.alessi@me.com>
2020-03-18 21:06:22 +0100

    Updates

---
+++ component---src-pages-index-js-048d1c3ced08d74cc5c8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(e,l,n){"use strict";n.r(l);var t=n("q1tI"),a=n.n(t),r=(n("Wbzz"),n("Bl7J")),o=n("vrFN");l.default=function(){return a.a.createElement(r.a,null,a.a.createElement(o.a,{title:"Home"}),a.a.createElement("h1",null),a.a.createElement("p",null,"Hey there! "),a.a.createElement("p",null,"The only thing you will see here will be what I do (or did) during Italy's lockdown."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-048d1c3ced08d74cc5c8.js.map
