95802a0ff03c8538a31b57ebe5167f611f6a4f4c
Federico <federico.alessi@me.com>
2020-03-20 20:21:20 +0100

    Update diary.js

--- quarantine-routines/src/pages/diary.js
+++ quarantine-routines/src/pages/diary.js
@@ -1,22 +1,38 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import DiaryEvents from "../components/diaryEvents";
 
 import SEO from "../components/seo"
+import {graphql} from "gatsby";
 
 const AboutPage = ({data}) => (
+
     <Layout>
-        <SEO title="About" />
+        <SEO title="Diary" />
         <h2>Storyline of the site</h2>
         <p>In this section I will post every changes I make day by day, starting from the day I deployed this site for the first time</p>
 
-        <p>
-            <h3>19th March 2020</h3>
-            Deployed this site with About, Diary and fedealessi96 sections. No post are visible, only the example one, used to test the page creation logic.
-        </p>
+        <DiaryEvents days={data.diaryPagesJson.days}/>
 
     </Layout>
+
 );
 
 
 export default AboutPage;
+
+
+export const query = graphql`
+  query {
+  diaryPagesJson {
+        days {
+            date
+            event
+        }
+    }
+  }
+
+`
+
+
