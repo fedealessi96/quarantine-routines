42598d3de1bfac95ea0bdc825bd66c14c429c6d2
Federico <federico.alessi@me.com>
2020-03-22 20:57:56 +0100

    Add rendering of md files

--- quarantine-routines/src/components/container.js
+++ quarantine-routines/src/components/container.js
@@ -1,11 +1,37 @@
 import React from "react";
 import {Container} from 'react-bootstrap';
+import {Link} from 'gatsby';
 
 const ContainerWrapper = (props) => {
-    console.log(props);
-    return (<Container>
-        <div>{props.data.allMarkdownRemark.totalCount} posts</div>
-    </Container>)
+    console.log(props.data);
+    const posts = props.data;
+
+
+    return (
+        <Container>
+            {posts.allMarkdownRemark.totalCount === 1 ? (<div>Only one post</div>) : (<div>{props.data.allMarkdownRemark.totalCount} total posts</div>)}
+
+            <div className="post-wrapper">{props.data.allMarkdownRemark.edges.map(({ node }) => {
+                console.log(node.frontmatter.date);
+                return(
+                    <div className="post" key={node.id}>
+                        <Link
+                            className={"post-title"}
+                            to={node.fields.slug}
+                        >
+                            <h3>{node.frontmatter.title}
+
+
+                                <span style={{color: '#555', float: "right"}}> ~ {node.frontmatter.date} </span>
+
+                            </h3>
+                        </Link>
+                </div>
+                );
+        })}
+        </div>
+        </Container>
+    )
 };
 
 export default ContainerWrapper;
